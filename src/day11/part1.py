1c63ac59db31ebf3628090eb053a45e49903008c
FusionSid <siddheshadsv@icloud.com>
2022-12-11 19:26:23 +1300

    feat(day): Finish Day 11, Parts 1 & 2

---
+++ src/day11/part1.py
@@ -0,0 +1,40 @@
+import re
+import math
+
+
+class Monkey:
+    def __init__(self, monkey_data: str):
+        self.items = list(map(int, re.findall("\\d+", monkey_data[1])))
+        self.operation = monkey_data[2].split("= ")[1]
+        self.divisible_by = int(re.findall("\\d+", monkey_data[3])[0])
+        self.throw_to = list(
+            map(int, re.findall("\\d+", monkey_data[4] + monkey_data[5]))
+        )
+        self.inspected = 0
+
+    def __repr__(self):
+        return f'({self.inspected}): {", ".join(map(str, self.items))}'
+
+
+with open("input.txt") as f:
+    monkey_notes = [i.split("\n") for i in f.read().split("\n\n")]
+    monkeys = [Monkey(monkey) for monkey in monkey_notes]
+
+ROUND_COUNT = 20
+
+for round in range(ROUND_COUNT):
+    for monkey in monkeys:
+        for item in monkey.items:
+            monkey.inspected += 1
+            worry = eval(monkey.operation, None, {"old": item}) // 3
+            if worry % monkey.divisible_by == 0:
+                monkeys[monkey.throw_to[0]].items.append(worry)
+                continue
+            monkeys[monkey.throw_to[1]].items.append(worry)
+        monkey.items = []
+
+
+active_monkeys = math.prod(sorted(map(lambda m: m.inspected, monkeys))[-2:])
+
+print("\n".join(f"Monkey {idx} {monkey}" for idx, monkey in enumerate(monkeys)))
+print("\nAnswer:", active_monkeys)
